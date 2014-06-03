7815e0e7e87d76b8d7000c3d2419db94fc565c47
norven63 <norven63@163.com>
2014-06-03 15:24:08 +0800

    1

--- book-lib/src/main/java/cn/retech/my_custom_network_engine/http_engine/async_http_client/HttpEngineOfAsyncHttpClient.java
+++ book-lib/src/main/java/cn/retech/my_custom_network_engine/http_engine/async_http_client/HttpEngineOfAsyncHttpClient.java
@@ -22,113 +22,119 @@ import com.loopj.android.http.RequestHandle;
 import com.loopj.android.http.RequestParams;
 
 public class HttpEngineOfAsyncHttpClient implements IHttpRequestForDomainBean, IHttpRequestForFile {
-  private final String TAG = this.getClass().getSimpleName();
-  public final AsyncHttpClient asyncHttpClient;
+	private final String TAG = this.getClass().getSimpleName();
+	public final AsyncHttpClient asyncHttpClient;
 
-  public HttpEngineOfAsyncHttpClient() {
-    asyncHttpClient = new AsyncHttpClient();
-    asyncHttpClient.setSSLSocketFactory(MySSLSocketFactory.getFixedSocketFactory());
-    asyncHttpClient.setUserAgent(ToolsFunctionForThisProgect.getUserAgent());
+	public HttpEngineOfAsyncHttpClient() {
+		asyncHttpClient = new AsyncHttpClient();
+		asyncHttpClient.setSSLSocketFactory(MySSLSocketFactory.getFixedSocketFactory());
+		asyncHttpClient.setUserAgent(ToolsFunctionForThisProgect.getUserAgent());
 
-    // PersistentCookieStore类用于实现Apache HttpClient的CookieStore接口，
-    // 可以自动的将cookie保存到Android设备的SharedPreferences中，如果你打算使用cookie来管理验证会话，
-    // 这个非常有用，因为用户可以保持登录状态，不管关闭还是重新打开你的app
-    //PersistentCookieStore myCookieStore = new PersistentCookieStore(GlobalDataCacheForMemorySingleton.getInstance.getApplication());
-  }
+		// PersistentCookieStore类用于实现Apache HttpClient的CookieStore接口，
+		// 可以自动的将cookie保存到Android设备的SharedPreferences中，如果你打算使用cookie来管理验证会话，
+		// 这个非常有用，因为用户可以保持登录状态，不管关闭还是重新打开你的app
+		// PersistentCookieStore myCookieStore = new
+		// PersistentCookieStore(GlobalDataCacheForMemorySingleton.getInstance.getApplication());
+	}
 
-  @Override
-  public INetRequestHandle requestDomainBean(final String url, final Map<String, String> dataDictionary, final IDomainBeanRequestAsyncHttpResponseListener domainBeanRequestAsyncHttpResponseListener) {
-    DebugLog.i(TAG, "短连接 : sendHttpRequest->url=" + url + ",dataDictionary=" + dataDictionary);
-    RequestHandle requestHandle = null;
+	@Override
+	public INetRequestHandle requestDomainBean(final String url, final Map<String, String> dataDictionary,
+			final IDomainBeanRequestAsyncHttpResponseListener domainBeanRequestAsyncHttpResponseListener) {
+		DebugLog.i(TAG, "短连接 : sendHttpRequest->url=" + url + ",dataDictionary=" + dataDictionary);
+		RequestHandle requestHandle = null;
 
-    // 定义异步HTTP网络响应处理监听器
-    final AsyncHttpResponseHandler responseHandler = new HttpRequestHandleOfAsyncHttpClient() {
-      @Override
-      public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-        domainBeanRequestAsyncHttpResponseListener.onSuccess(this, response);
+		// 定义异步HTTP网络响应处理监听器
+		final AsyncHttpResponseHandler responseHandler = new HttpRequestHandleOfAsyncHttpClient() {
+			@Override
+			public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+				domainBeanRequestAsyncHttpResponseListener.onSuccess(this, response);
 
-        this.setFinished(true);
-      }
+				this.setFinished(true);
+			}
 
-      @Override
-      public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-        domainBeanRequestAsyncHttpResponseListener.onFailure(this, statusCode, e);
+			@Override
+			public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+				domainBeanRequestAsyncHttpResponseListener.onFailure(this, statusCode, e);
 
-        this.setFinished(true);
-      }
+				this.setFinished(true);
+			}
 
-    };
+		};
 
-    // 如果dataDictionary不为空, 我们就使用 POST方式发送数据到服务器, 否则就是用GET方式访问服务器
-    if (dataDictionary != null && dataDictionary.size() > 0) {
-      RequestParams requestParams = (RequestParams) EngineHelperSingleton.getInstance.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(dataDictionary);
-      requestHandle = asyncHttpClient.post(url, requestParams, responseHandler);
-    } else {
-      requestHandle = asyncHttpClient.get(url, responseHandler);
-    }
+		// 如果dataDictionary不为空, 我们就使用 POST方式发送数据到服务器, 否则就是用GET方式访问服务器
+		if (dataDictionary != null && dataDictionary.size() > 0) {
+			RequestParams requestParams = (RequestParams) EngineHelperSingleton.getInstance
+					.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(dataDictionary);
+			requestHandle = asyncHttpClient.post(url, requestParams, responseHandler);
+		} else {
+			requestHandle = asyncHttpClient.get(url, responseHandler);
+		}
 
-    // 定义 AsyncHttpClient 定制的 HttpRequestHandle
-    HttpRequestHandleOfAsyncHttpClient httpRequestHandleOfAsyncHttpClient = (HttpRequestHandleOfAsyncHttpClient) responseHandler;
-    httpRequestHandleOfAsyncHttpClient.setRequestHandle(requestHandle);
-    httpRequestHandleOfAsyncHttpClient.setFinished(false);
-    return httpRequestHandleOfAsyncHttpClient;
-  }
+		// 定义 AsyncHttpClient 定制的 HttpRequestHandle
+		HttpRequestHandleOfAsyncHttpClient httpRequestHandleOfAsyncHttpClient = (HttpRequestHandleOfAsyncHttpClient) responseHandler;
+		httpRequestHandleOfAsyncHttpClient.setRequestHandle(requestHandle);
+		httpRequestHandleOfAsyncHttpClient.setFinished(false);
+		return httpRequestHandleOfAsyncHttpClient;
+	}
 
-  @Override
-  public INetRequestHandle requestFile(final String url, final boolean isNeedContinuingly, final Map<String, String> dataDictionary, final File downLoadFile,
-      final IFileRequestAsyncHttpResponseListener fileRequestAsyncHttpResponseListener) {
-    DebugLog.i(TAG, "文件请求 : sendHttpRequest->url=" + url + ",dataDictionary=" + dataDictionary);
+	@Override
+	public INetRequestHandle requestFile(final String url, final boolean isNeedContinuingly,
+			final Map<String, String> dataDictionary, final File downLoadFile,
+			final IFileRequestAsyncHttpResponseListener fileRequestAsyncHttpResponseListener) {
+		DebugLog.i(TAG, "文件请求 : sendHttpRequest->url=" + url + ",dataDictionary=" + dataDictionary);
 
-    // effective for java 38 检查参数有效性, 对于共有的方法,
-    // 要使用异常机制来通知调用方发生了入参错误.
-    if (TextUtils.isEmpty(url)) {
-      throw new NullPointerException("入参为空.");
-    }
-    RequestHandle requestHandle = null;
+		// effective for java 38 检查参数有效性, 对于共有的方法,
+		// 要使用异常机制来通知调用方发生了入参错误.
+		if (TextUtils.isEmpty(url)) {
+			throw new NullPointerException("入参为空.");
+		}
+		RequestHandle requestHandle = null;
 
-    long seekPos = 0;
-    asyncHttpClient.removeHeader("Range");
-    if (isNeedContinuingly) {
-      // 需要断点续传
-      if (downLoadFile.exists()) {
-        asyncHttpClient.addHeader("Range", "bytes=" + downLoadFile.length() + "-");
-        seekPos = downLoadFile.length();
-      }
-    } else {
-      // 不需要断点续传时, 要删除之前的临时文件, 好重头进行下载
-      seekPos = 0;
-    }
+		long seekPos = 0;
+		asyncHttpClient.removeHeader("Range");
+		if (isNeedContinuingly) {
+			// 需要断点续传
+			if (downLoadFile.exists()) {
+				asyncHttpClient.addHeader("Range", "bytes=" + downLoadFile.length() + "-");
+				seekPos = downLoadFile.length();
+			}
+		} else {
+			// 不需要断点续传时, 要删除之前的临时文件, 好重头进行下载
+			seekPos = 0;
+		}
 
-    try {
-      final RandomAccessFileAsyncHttpResponseHandler fileAsyncHttpResponseHandler = new RandomAccessFileAsyncHttpResponseHandler(downLoadFile, seekPos) {
+		try {
+			final RandomAccessFileAsyncHttpResponseHandler fileAsyncHttpResponseHandler = new RandomAccessFileAsyncHttpResponseHandler(
+					downLoadFile, seekPos) {
 
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, File file) {
-          fileRequestAsyncHttpResponseListener.onSuccess(file);
-        }
+				@Override
+				public void onSuccess(int statusCode, Header[] headers, File file) {
+					fileRequestAsyncHttpResponseListener.onSuccess(file);
+				}
 
-        @Override
-        public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-          fileRequestAsyncHttpResponseListener.onFailure(statusCode, e);
-        }
+				@Override
+				public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+					fileRequestAsyncHttpResponseListener.onFailure(statusCode, e);
+				}
 
-        @Override
-        public void onProgress(int bytesWritten, int totalSize) {
-          fileRequestAsyncHttpResponseListener.onProgress(bytesWritten, totalSize);
-        }
+				@Override
+				public void onProgress(int bytesWritten, int totalSize) {
+					fileRequestAsyncHttpResponseListener.onProgress(bytesWritten, totalSize);
+				}
 
-      };
-      // 如果dataDictionary不为空, 我们就使用 POST方式发送数据到服务器, 否则就是用GET方式访问服务器
-      if (dataDictionary != null && dataDictionary.size() > 0) {
-        RequestParams requestParams = (RequestParams) EngineHelperSingleton.getInstance.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(dataDictionary);
-        requestHandle = asyncHttpClient.post(url, requestParams, fileAsyncHttpResponseHandler);
-      } else {
-        requestHandle = asyncHttpClient.get(url, fileAsyncHttpResponseHandler);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+			};
+			// 如果dataDictionary不为空, 我们就使用 POST方式发送数据到服务器, 否则就是用GET方式访问服务器
+			if (dataDictionary != null && dataDictionary.size() > 0) {
+				RequestParams requestParams = (RequestParams) EngineHelperSingleton.getInstance
+						.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(dataDictionary);
+				requestHandle = asyncHttpClient.post(url, requestParams, fileAsyncHttpResponseHandler);
+			} else {
+				requestHandle = asyncHttpClient.get(url, fileAsyncHttpResponseHandler);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    return new HttpRequestHandleOfAsyncHttpClient(requestHandle);
-  }
+		return new HttpRequestHandleOfAsyncHttpClient(requestHandle);
+	}
 }
